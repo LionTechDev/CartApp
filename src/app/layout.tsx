e8d0cdb7e09bc68dbb4c2a91aedde0bcc37f2848
Emilio Suárez <91371538+emisua@users.noreply.github.com>
2024-09-19 21:03:08 +0200

    change in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-	title: "Cart App",
-	description: "the firs cart app focus on marketing",
-};
+  title: 'Cart App',
+  description: 'the firs cart app focus on marketing',
+}
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode
 }>) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang='en'>
+      <p>esto es un layout</p>
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
 }
