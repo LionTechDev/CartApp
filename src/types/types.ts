a402fe9678230c48b42c59c6cd7fc94a22226d29
patricioreguera <reguerapatriciohernan@gmail.com>
2024-09-21 22:56:17 +0200

    create new interfaces

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,3 +5,40 @@ export interface Menu {
   image?: string
   isActive: boolean
 }
+
+export interface Category {
+  menu_id: string;
+	category_id: string;
+	title: string;
+	image?: string ;
+	products?: Product[];
+}
+export interface Product {
+  category_id: string[];
+	product_id: string;
+	title: string;
+	price: string;
+	description?: string;
+	image?: string;
+  allergen?: Allergen[];
+}
+
+export type Allergen = 
+  | 'Gluten'
+  | 'Crustáceos'
+  | 'Huevos'
+  | 'Pescado'
+  | 'Cacahuetes'
+  | 'Soja'
+  | 'Leche'
+  | 'Frutos secos'
+  | 'Apio'
+  | 'Mostaza'
+  | 'Sésamo'
+  | 'Sulfitos'
+  | 'Lupino'
+  | 'Mariscos'
+  | 'Frutas cítricas'
+  | 'Miel'
+  | 'Altramuces'
+;
