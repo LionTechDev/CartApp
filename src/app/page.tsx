b90c9fb8d4ff2da7faf0686be00b251c5624f293
patricioreguera <reguerapatriciohernan@gmail.com>
2024-09-22 03:41:47 +0200

    fix: login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 import React from "react";
-import { addTextToDb } from "./actions";
 
 const Home = () => {
 	return (
 		<form>
 			<input type="text" name="text" id="text" className="border" />
-			<button formAction={addTextToDb} className="bg-orange-400">
-				Agregar texto a la db
-			</button>
 		</form>
 	);
 };
