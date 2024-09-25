7e3e27d0fd1d192342d6d1f0cc62ccec5faa2c19
patricioreguera <reguerapatriciohernan@gmail.com>
2024-09-25 21:08:05 +0200

    Feat: Home page button link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
 "use client";
+import Link from "next/link";
 import React from "react";
 
 const Home = () => {
 	return (
-		<form>
-			<input type="text" name="text" id="text" className="border" />
-		</form>
+		<div className="flex flex-col items-center  gap-5">
+			<h1>Welcome Page</h1>
+			<Link
+				href={"/dashboard"}
+				className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+			>
+				Ir al dashboard
+			</Link>
+			<Link
+				href={"/login"}
+				className="bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded"
+			>
+				Ir al login
+			</Link>
+		</div>
 	);
 };
 
