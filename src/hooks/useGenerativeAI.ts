db992a2badc5d4cf0b4463b9892d496636937d0d
patricioreguera <reguerapatriciohernan@gmail.com>
2024-10-04 20:06:15 +0200

    Fix: api

--- src/hooks/useGenerativeAI.ts
+++ src/hooks/useGenerativeAI.ts
@@ -29,7 +29,7 @@ export const useGenerativeAI = () => {
         },
       ];
 
-const genAI = new GoogleGenerativeAI(API_KEY);
+const genAI = new GoogleGenerativeAI(API_KEY || "");
       
       const model = genAI.getGenerativeModel({
         model: "gemini-1.5-flash",
